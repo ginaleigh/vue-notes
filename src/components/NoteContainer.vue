a69feec7b68898ba987f723c281e53236a08b71f
Gina Mirando <gina.mirando5@gmail.com>
2022-08-03 19:41:29 -0500

    practice/exploring the concept of components

---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,13 @@
+<script>
+import NoteSelector from ".NoteSelectors";
+export default {
+  components: {
+    NoteSelector,
+  },
+};
+</script>
+<template>
+  <div class="note-container">
+    <div class="note-editor"></div>
+  </div>
+</template>
